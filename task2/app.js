291da33c71516b1c4887d1434a1137055485aa36
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-05-15 11:41:56 +0400

    Task2 finis

--- task2/app.js
+++ task2/app.js
@@ -1,1 +1,16 @@
-console.log("fiko");
+function swapCase(string) {
+  let swappedStr = "";
+  for (let i = 0; i < string.length; i++) {
+    let element = string[i];
+    if (element === element.toUpperCase()) {
+      swappedStr += element.toLowerCase();
+    } else {
+      swappedStr += element.toUpperCase();
+    }
+  }
+  return swappedStr;
+}
+
+let text = "jeDAcAdemY";
+let swappedText = swapCase(text);
+console.log(swappedText);
